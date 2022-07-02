047d3ab3e2395fc4035d9d782a5470c9f325244c
oF <sky0932039612@gmail.com>
2022-07-02 08:58:25 +0800

    新增漢語轉拼音轉換器

---
+++ commands/pinyin.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const { pinyin } = require("pinyin-pro")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('pinyin')
+        .setDescription('漢語轉拼音轉換器')
+        .addStringOption((option) => option.setName('text').setDescription('漢語').setRequired(true)),
+    async execute(interaction) {
+        const original = interaction.options.getString('text');
+
+        const pinyinEmbed = new MessageEmbed()
+            .setTitle('漢語轉拼音轉換器')
+            .setDescription(`漢語：${original}\n拼音：${pinyin(original, { toneType: 'num', removeNonZh: true })}`)
+            .setColor('#5e9bbc')
+            .setFooter({ text: 'Copyright © 2022 oF' });
+
+        await interaction.reply({ embeds: [pinyinEmbed] });
+    }
+};
