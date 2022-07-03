ec51a1f3f5bdb5bddbba58356717a0dbb12db3cd
oF <sky0932039612@gmail.com>
2022-07-03 09:40:08 +0800

    新增手搖飲料菜單

---
+++ commands/drink-menu-add.js
@@ -0,0 +1,83 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
+const isImage = require("is-image");
+const dayjs = require('dayjs');
+const mysql = require('mysql');
+const { DrinkMenu } = require('../assets/permission');
+
+require('dotenv').config();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('drink-menu-add')
+        .setDescription('新增手搖飲料菜單')
+        .addStringOption((option) => option.setName('name').setDescription('店名').setRequired(true))
+        .addStringOption((option) => option.setName('menu').setDescription('菜單').setRequired(true)),
+    async execute(interaction) {
+        if (DrinkMenu.Add.includes(interaction.user.id)) {
+            const connection = mysql.createConnection({
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: 'discord-bot'
+            });
+
+            connection.connect();
+
+            const name = interaction.options.getString('name');
+            const menu = interaction.options.getString('menu');
+
+            if (isImage(menu)) {
+                const menuCheckSql = `SELECT * FROM drink_menu WHERE name LIKE '%${name}%' LIMIT 1`;
+
+                const now = dayjs(new Date()).format('YYYY/MM/DD HH:mm:ss') ;
+                const menuAddSql = `INSERT INTO drink_menu VALUES ('${name}', '${menu}', '${now}', '${interaction.user.id}')`;
+
+                connection.query(menuCheckSql, async (error, result) => {
+                    if (typeof(result[0]) === 'undefined') {
+                        connection.query(menuAddSql, async (error) => {
+                            if (!error) {
+                                const drinkEmbed = new MessageEmbed()
+                                    .setTitle('手搖飲料菜單')
+                                    .setDescription(`${name} - 菜單新增成功!`)
+                                    .setColor('#ffcc80')
+                                    .setImage(menu)
+                                    .setFooter({ text: 'Copyright © 2022 oF' });
+
+                                await interaction.reply({ embeds: [drinkEmbed] });
+                            } else {
+                                const drinkEmbed = new MessageEmbed()
+                                    .setTitle('手搖飲料菜單')
+                                    .setDescription(`${name} - 菜單新增失敗!\n${error}`)
+                                    .setColor('#f23857')
+                                    .setFooter({ text: 'Copyright © 2022 oF' });
+
+                                await interaction.reply({ embeds: [drinkEmbed] });
+                            }
+                        });
+                    } else {
+                        const drinkEmbed = new MessageEmbed()
+                            .setTitle('手搖飲料菜單')
+                            .setDescription(`${name} - 菜單已存在!`)
+                            .setColor('#f23857')
+                            .setFooter({ text: 'Copyright © 2022 oF' });
+
+                        await interaction.reply({ embeds: [drinkEmbed] });
+                    }
+                });
+            } else {
+                const drinkEmbed = new MessageEmbed()
+                    .setTitle('手搖飲料菜單')
+                    .setDescription(`請確認菜單網址是否正確!`)
+                    .setColor('#f23857')
+                    .setFooter({ text: 'Copyright © 2022 oF' });
+
+                await interaction.reply({ embeds: [drinkEmbed] });
+            }
+
+            connection.end();
+        } else {
+            await interaction.reply({ content: '你沒有權限使用這個指令!' });
+        }
+    }
+};
--- commands/drink-menu.js
+++ commands/drink-menu.js
@@ -1,7 +1,7 @@
-const path = require("node:path");
-const fs = require("node:fs");
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageAttachment, MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const mysql = require('mysql');
+const dayjs = require("dayjs");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,32 +9,63 @@ module.exports = {
         .setDescription('查詢手搖飲料菜單')
         .addStringOption((option) => option.setName('name').setDescription('店名').setRequired(true)),
     async execute(interaction) {
-        const name = interaction.options.getString('name');
-        const pathArr = path.join(__dirname, '../assets/drinkMenu');
-        const fileArr = fs.readdirSync(pathArr).filter(file => file.endsWith('.png'));
+        const connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: 'discord-bot'
+        });
 
-        const getDrinkEmbed = async () => {
-            for (const file of fileArr) {
-                if (file.toLowerCase().includes(name.toLowerCase())) {
-                    const filePath = path.join(pathArr, file);
-                    const attachment = new MessageAttachment(filePath, 'name.png');
+        connection.connect();
+
+        const name = interaction.options.getString('name');
+
+        if (name.length >= 2) {
+            const menuSearchSql = `SELECT * FROM drink_menu WHERE name LIKE '%${name}%' LIMIT 1`;
+
+            connection.query(menuSearchSql, async (error, result) => {
+                if (!error) {
+                    if (typeof(result) !== 'undefined') {
+                        const date = dayjs(result[0].date).format('YYYY/MM/DD HH:mm:ss Z[Z]');
+
+                        const drinkEmbed = new MessageEmbed()
+                            .setTitle('手搖飲料菜單')
+                            .setDescription(`店名: ${result[0].name}\n`)
+                            .addField('最後更新時間', date)
+                            .setColor('#ffcc80')
+                            .setImage(result[0].image)
+                            .setFooter({ text: 'Copyright © 2022 oF' });
+
+                        await interaction.reply({ embeds: [drinkEmbed] });
+                    } else {
+                        const drinkEmbed = new MessageEmbed()
+                            .setTitle('手搖飲料菜單')
+                            .setDescription('目前尚無菜單!')
+                            .setColor('#ff4444')
+                            .setFooter({ text: 'Copyright © 2022 oF' });
+
+                        await interaction.reply({ embeds: [drinkEmbed] });
+                    }
+                } else {
                     const drinkEmbed = new MessageEmbed()
                         .setTitle('手搖飲料菜單')
-                        .setDescription(`店名: ${file.replace('.png', '')}`)
-                        .setColor('#ffcc80')
-                        .setImage('attachment://name.png')
-                    await interaction.reply({ embeds: [drinkEmbed], files: [attachment] });
-                    return true;
-                }
-            }
-        }
+                        .setDescription(error)
+                        .setColor('#ff4444')
+                        .setFooter({ text: 'Copyright © 2022 oF' });
 
-        if (!await getDrinkEmbed()) {
+                    await interaction.reply({ embeds: [drinkEmbed] });
+                }
+            });
+        } else {
             const drinkEmbed = new MessageEmbed()
                 .setTitle('手搖飲料菜單')
-                .setDescription('目前尚未有菜單!')
+                .setDescription('請輸入2位以上的關鍵字進行查詢!')
                 .setColor('#ff4444')
+                .setFooter({ text: 'Copyright © 2022 oF' });
+
             await interaction.reply({ embeds: [drinkEmbed] });
         }
+
+        connection.end();
     }
 };
